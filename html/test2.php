96b8152554921ddfbbcf0787e1dd98cd347ebe0f
yana-ship <oselot_f620@docomo.ne.jp>
2022-10-30 23:26:10 +0900

    かにうにタコイカ追加

---
+++ html/test2.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja"><head>
+        <meta charset="UTF-8">
+        <title></title>
+        <meta name="description" content="taisei miyaji profile">
+
+        <!-- CSS -->
+        <link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css">
+        <link href="https://fonts.googleapis.com/css?family=Philosopher" rel="stylesheet">
+        <link href="css/style.css" rel="stylesheet">
+        <!-- <link rel="stylesheet" href="https://taiseimiyaji.github.io/Prolile/css/style.css"> -->
+        <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+        <script src="js/contact.js"></script>
+    </head>
+
+    <body>
+
+    </body>
+    </html>
